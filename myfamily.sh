715694e06e15039c73bcf64cb554638ede2f964c
dyebecks <damir.yerkin@gmail.com>
2019-10-15 22:56:38 +0600

    update

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -r --arg v "HERO_ID" '.[] | select(.id==$v) | .connections | .relatives'
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -r --arg "HERO_ID" '.[] | select(.id==$HERO_ID) | .connections | .relatives'
