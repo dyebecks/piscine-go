bf2a54ed5e0303f4b7f70757e52b216917016ac1
dyebecks <damir.yerkin@gmail.com>
2019-10-15 23:31:58 +0600

    update

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,1 @@
-echo $HERO_ID
-curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -r '.[] | select(.id==1) | .connections | .relatives'
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -r '.[] | select(.id==$HERO_ID) | .connections | .relatives'
