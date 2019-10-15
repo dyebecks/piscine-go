9031ce671513408b4c67e7585f556a003dca72a4
dyebecks <damir.yerkin@gmail.com>
2019-10-15 20:42:46 +0600

    new task family

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -r '.[] | select(.id==1) | .connections | .relatives'
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -r '.[] | select(.id==70) | .connections | .relatives'
