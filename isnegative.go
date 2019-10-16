3deb67aa4504e0fbf472d945f38c7d08afec0a23
dyebecks <damir.yerkin@gmail.com>
2019-10-16 21:15:49 +0600

    new task

--- isnegative.go
+++ isnegative.go
@@ -3,9 +3,10 @@ package main
 import piscine "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
-	if nb<0 {z01.printLn("T")
-}
-	else {z01.printLn("F")}
-}
-z01.PrintRune('\n')
+	if nb < 0 {
+		z01.printLn("T")
+	} else {
+		z01.printLn("F")
+	}
+	z01.PrintRune('\n')
 }
