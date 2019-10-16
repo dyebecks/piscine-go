c36355ce55ae6af71982504ffc01a24eae384d7e
dyebecks <damir.yerkin@gmail.com>
2019-10-16 20:41:35 +0600

    update

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -1,12 +1,11 @@
 package main
 
 import "github.com/01-edu/z01"
-import "fmt"
 
 func main() {
-	for i := 122; i >= 97; {
+	for i := 'z'; i >= 'a'; {
 		z01.PrintRune(i)
 		i--
 	}
-	fmt.Println('\n')
+	z01.PrintRune(10)
 }
